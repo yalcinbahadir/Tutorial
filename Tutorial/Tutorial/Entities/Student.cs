2a7bfe66bc22e4554b701ecd124a5f57ba8ca6e8
Bahadir Yalcin <yalcin.bahadirali@gmail.com>
2022-05-01 00:54:33 +0200

    ali int

--- Tutorial/Tutorial/Entities/Student.cs
+++ Tutorial/Tutorial/Entities/Student.cs
@@ -10,6 +10,7 @@ namespace Tutorial.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Ali { get; set; }
     }
 
     public class Person
